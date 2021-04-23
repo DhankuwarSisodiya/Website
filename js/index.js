35d7a53fc27d81845117f2fbb3314e71ddb130e3
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-04-22 22:53:54 -0400

    Animations

--- js/index.js
+++ js/index.js
@@ -1,19 +1,4 @@
 const html = document.documentElement;
 $(document).ready(function () {
-    var $horizontal = $('#horizontal');
-    
-    $(window).scroll(function () {
-        var s = $(this).scrollTop(),
-            d = $(document).height(),
-            c = $(this).height();
 
-        scrollPercent = (s / (d - c));
-        if(s >= c){
-            var position = (scrollPercent * ($(document).width()));
-            $horizontal.css({
-                'right': position
-            });
-            console.log(s,d,c,position)
-        }
-    });
 });
