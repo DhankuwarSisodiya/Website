a4537bf4f1dccf5559546ac9b4947182547d4292
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-08-28 21:41:14 -0400

    section 3

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const html = document.documentElement;
 $(document).ready(function () {
     
     let experience = document.getElementById("experience");
+    let projects = document.getElementById("projects");
+    projects.style.display = "none"
     experience.scrollLeft = 1
     var scroller = {};
     scroller.e = document.getElementById("experience");
@@ -16,8 +18,9 @@ $(document).ready(function () {
 function MouseWheelHandler(e) {
     let experience = document.getElementById("experience");
     let projects = document.getElementById("projects");
+    projects.style.display = "none"
     //experience has reached top & exprience right has not reached yet -> activate horizontal scrolling
-    // console.log(window.pageYOffset,window.innerHeight+50, " and ",experience.scrollLeft, experience.clientWidth, experience.scrollWidth)
+    console.log(window.pageYOffset,window.innerHeight+50, " and ",experience.scrollLeft, experience.clientWidth, experience.scrollWidth)
     if(window.pageYOffset > window.innerHeight+50 && (experience.scrollLeft+experience.clientWidth <= experience.scrollWidth) && experience.scrollLeft > 0){
         var e = window.event || e;
         var delta = - 20 * (Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))));
@@ -25,18 +28,27 @@ function MouseWheelHandler(e) {
         var pst = experience.scrollLeft + delta;
 
         if (pst < 0) {
-            // console.log("GOING RIGHT")
+            console.log("GOING RIGHT")
             pst = 0;
         } else if (pst > experience.scrollWidth) {
-            // console.log("GOING LEFT")
-            pst = experience.scrollWidth;
+            console.log("GOING LEFT")
+            pst = experience.scrollWidth*0.1;
         }
-        // console.log(pst)
+        console.log(pst)
         experience.scrollLeft = pst;
         e.preventDefault()
+        if(experience.scrollLeft == 762){
+            console.log("in here")
+            projects.style.display = "block"
+            window.scroll({
+                top: window.innerHeight*2,
+                left: 0,
+                behavior: 'smooth'
+              });
+        }
         return false;
     }
-
+    
     if(window.pageYOffset < window.innerHeight+50){
         experience.scrollLeft = 1
     }
